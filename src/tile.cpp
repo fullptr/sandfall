2880765d7482902532490dea5355f09b5335a324
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-05-16 22:20:26 +0100

    Reset sand velocity when moving sideways

--- src/tile.cpp
+++ src/tile.cpp
@@ -44,14 +44,13 @@ void tile::update_sand(pixel_api&& api, double dt)
     auto& vel = api.get({0, 0}).velocity;
     vel += api.world_settings().gravity * (float)dt;
     glm::ivec2 offset{0, glm::max(1, (int)vel.y)};
-    
+
     if (api.move_to(offset)) {
         return;
     }
 
     std::array<glm::ivec2, 2> offsets = {
-        glm::ivec2{-1, 1},
-        glm::ivec2{1, 1},
+        glm::ivec2{-1, 1}, glm::ivec2{1, 1},
     };
 
     if (rand() % 2) {
@@ -60,8 +59,9 @@ void tile::update_sand(pixel_api&& api, double dt)
 
     for (auto offset : offsets) {
         if (api.move_to(offset)) {
-            if (offset.y == 0) { api.get(offset).velocity = {0.0, 0.0}; }
             return;
+        } else {
+            api.get({0, 0}).velocity = {0.0, 0.0};
         }
     }
 }
@@ -77,10 +77,7 @@ void tile::update_water(pixel_api&& api, double dt)
     }
 
     std::array<glm::ivec2, 4> offsets = {
-        glm::ivec2{-1, 1},
-        glm::ivec2{1, 1},
-        glm::ivec2{-1, 0},
-        glm::ivec2{1, 0}
+        glm::ivec2{-1, 1}, glm::ivec2{1, 1}, glm::ivec2{-1, 0}, glm::ivec2{1, 0}
     };
 
     if (rand() % 2) {
