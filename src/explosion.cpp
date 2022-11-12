8793b671e789b7b60a2e762af61823663c4b2e68
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2022-11-12 12:00:17 +0000

    Fixed scorching blocking pixels, but the code is bad

--- src/explosion.cpp
+++ src/explosion.cpp
@@ -21,17 +21,24 @@ auto explosion_ray(
     const auto b = a + glm::ivec2{(info.max_radius + info.scorch_radius) * dir};
     const auto steps = glm::max(glm::abs(a.x - b.x), glm::abs(a.y - b.y));
 
+    const auto blast_limit = random_from_range(info.min_radius, info.max_radius);
     auto scorch_limit = std::optional<float>{};
 
     for (int i = 0; i < steps; ++i) {
         const auto curr = a + (b - a) * i/steps;
-        const auto curr_radius = (static_cast<float>(i) / steps) * glm::length(glm::vec2{b});
+        const auto curr_radius = glm::length(glm::vec2{curr} - glm::vec2{pos});
+
+        if (!pixels.valid(curr)) {
+            return;
+        }
 
         if (scorch_limit.has_value()) {
             if (curr_radius >= *scorch_limit) {
                 return;
             }
-            pixels.at(curr).colour *= 0.8f;
+            if (properties(pixels.at(curr)).phase == pixel_phase::solid) {
+                pixels.at(curr).colour *= 0.8f;
+            }
             continue;
         }
 
@@ -45,13 +52,27 @@ auto explosion_ray(
                 return;
             }
             if (pixels.at(curr + offset).type == pixel_type::titanium) {
-                scorch_limit = curr_radius + info.scorch_radius;
+                scorch_limit = curr_radius + random_from_range(0.0f, info.scorch_radius);
+                pixels.at(curr + offset).colour *= 0.8f;
+                break;
+            }
+            if (curr_radius >= blast_limit) {
+                scorch_limit = curr_radius + random_from_range(0.0f, info.scorch_radius);
+                break;
+            }
+            if (scorch_limit.has_value()) {
+                if (curr_radius >= *scorch_limit) {
+                    return;
+                }
+                if (properties(pixels.at(curr)).phase == pixel_phase::solid) {
+                    pixels.at(curr).colour *= 0.8f;
+                }
                 break;
             }
 
             pixels.set(curr + offset, random_unit() < 0.05f ? pixel::ember() : pixel::air());
+            checked.emplace(curr);
         }
-        checked.emplace(curr);
     }
 }
 
