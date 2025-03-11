4aa18cacfe00a3313521dda6bd745d792dd87eed
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2025-03-11 22:21:49 +0000

    Remove some of the headers that are no longer used

--- src/sandfall.m.cpp
+++ src/sandfall.m.cpp
@@ -18,11 +18,9 @@
 #include <glm/gtx/norm.hpp>
 #include <box2d/box2d.h>
 #include <cereal/archives/binary.hpp>
-
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-#include <imgui-SFML.h>
 
 #include <memory>
 #include <print>
