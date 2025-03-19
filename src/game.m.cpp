88c9728ddbb812043734447274a1292d1d3b54fa
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2025-03-19 00:39:08 +0000

    Remove imgui from game.m.cpp

--- src/game.m.cpp
+++ src/game.m.cpp
@@ -17,10 +17,6 @@
 #include <glm/gtx/norm.hpp>
 #include <box2d/box2d.h>
 #include <cereal/archives/binary.hpp>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#include <imgui_stdlib.h>
 
 #include <memory>
 #include <print>
@@ -151,12 +147,6 @@ auto main() -> int
         .world_to_screen = 720.0f / 256.0f
     };
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(window.native_handle(), true);
-    ImGui_ImplOpenGL3_Init("#version 410");
-
     while (window.is_running()) {
         const double dt = timer.on_update();
         window.begin_frame();
@@ -164,14 +154,6 @@ auto main() -> int
         keyboard.on_new_frame();
 
         for (const auto event : window.events()) {
-            auto& io = ImGui::GetIO();
-            if (event.is_keyboard_event() && io.WantCaptureKeyboard) {
-                continue;
-            }
-            if (event.is_mount_event() && io.WantCaptureMouse) {
-                continue;
-            }
-
             mouse.on_event(event);
             keyboard.on_event(event);
 
@@ -231,103 +213,9 @@ auto main() -> int
                     updated = true;
                 }
         }
-        
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
 
         const auto mouse_actual = mouse_pos_world_space(mouse, camera);
         const auto mouse_pixel = pixel_at_mouse(mouse, camera);
-
-        ImGui::ShowDemoWindow(&editor.show_demo);
-
-        if (ImGui::Begin("Editor")) {
-            ImGui::Text("Mouse");
-            ImGui::Text("Position: {%.2f, %.2f}", mouse_actual.x, mouse_actual.y);
-            ImGui::Text("Position: {%d, %d}", (int)std::round(mouse_actual.x), (int)std::round(mouse_actual.y));
-            ImGui::Text("Pixel: {%d, %d}", mouse_pixel.x, mouse_pixel.y);
-            if (level->pixels.valid(mouse_pixel)) {
-                const auto px = level->pixels[mouse_pixel];
-                ImGui::Text("  pixel power: %d", px.power);
-            }
-            ImGui::Text("Number of Floors: %d", level->player.floors().size());
-            ImGui::Text("Events this frame: %zu", window.events().size());
-            ImGui::Separator();
-
-            ImGui::Text("Camera");
-            ImGui::Text("Top Left: {%.2f, %.2f}", camera.top_left.x, camera.top_left.y);
-            ImGui::Text("Screen width: %.2f", camera.screen_width);
-            ImGui::Text("Screen height: %.2f", camera.screen_height);
-            ImGui::Text("Scale: %f", camera.world_to_screen);
-
-            ImGui::Separator();
-            ImGui::Checkbox("Show Physics", &editor.show_physics);
-            ImGui::Checkbox("Show Spawn", &editor.show_spawn);
-            ImGui::SliderInt("Spawn X", &level->spawn_point.x, 0, level->pixels.width());
-            ImGui::SliderInt("Spawn Y", &level->spawn_point.y, 0, level->pixels.height());
-            if (ImGui::Button("Respawn")) {
-                level->player.set_position(level->spawn_point);
-            }
-            ImGui::Separator();
-
-            ImGui::Text("Info");
-            ImGui::Text("FPS: %d", timer.frame_rate());
-            ImGui::Text("Awake chunks: %d", std::count_if(level->pixels.chunks().begin(), level->pixels.chunks().end(), [](const sand::chunk& c) {
-                return c.should_step;
-            }));
-            ImGui::Checkbox("Show chunks", &editor.show_chunks);
-            if (ImGui::Button("Clear")) {
-                level->pixels.wake_all();
-                std::fill(level->pixels.begin(), level->pixels.end(), sand::pixel::air());
-            }
-            ImGui::Separator();
-
-            ImGui::Text("Brush");
-            ImGui::SliderFloat("Size", &editor.brush_size, 0, 50);
-            if (ImGui::RadioButton("Spray", editor.brush_type == 0)) editor.brush_type = 0;
-            if (ImGui::RadioButton("Square", editor.brush_type == 1)) editor.brush_type = 1;
-            if (ImGui::RadioButton("Explosion", editor.brush_type == 2)) editor.brush_type = 2;
-
-            for (std::size_t i = 0; i != editor.pixel_makers.size(); ++i) {
-                if (ImGui::Selectable(editor.pixel_makers[i].first.c_str(), editor.current == i)) {
-                    editor.current = i;
-                }
-            }
-            ImGui::Separator();
-            ImGui::InputInt("chunk width", &editor.new_world_chunks_width);
-            ImGui::InputInt("chunk height", &editor.new_world_chunks_height);
-            if (ImGui::Button("New World")) {
-                level = new_level(editor.new_world_chunks_width, editor.new_world_chunks_height);
-                updated = true;
-            }
-            ImGui::Text("Levels");
-            for (int i = 0; i != 5; ++i) {
-                ImGui::PushID(i);
-                const auto filename = std::format("save{}.bin", i);
-                if (ImGui::Button("Save")) {
-                    save_level(filename, *level);
-                }
-                ImGui::SameLine();
-                if (ImGui::Button("Load")) {
-                    level = load_level(filename);
-                    updated = true;
-                }
-                ImGui::SameLine();
-                ImGui::Text("Save %d", i);
-                ImGui::PopID();
-            }
-            static std::string filepath;
-            ImGui::InputText("Load PNG", &filepath);
-            if (ImGui::Button("Try Load")) {
-                std::ifstream ifs{filepath, std::ios_base::in | std::ios_base::binary};
-                std::vector<char> buffer(
-                    (std::istreambuf_iterator<char>(ifs)),
-                    std::istreambuf_iterator<char>()
-                );
-                std::print("loaded a file containing {} bytes\n", buffer.size());
-            }
-        }
-        ImGui::End();
 
         // Render and display the world
         world_renderer.bind();
@@ -352,16 +240,9 @@ auto main() -> int
 
         shape_renderer.end_frame();
         
-        ImGui::EndFrame();
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         window.end_frame();
     }
-
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
     
     return 0;
 }
