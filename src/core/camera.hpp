101d3d96140cac277c9554e6b03ad32cd7354a4f
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2025-03-19 01:28:11 +0000

    Add helper function to camera header

--- src/core/camera.hpp
+++ src/core/camera.hpp
@@ -11,4 +11,10 @@ struct camera
     float world_to_screen; // the number of screen pixels per world pixel, multiplying by it takes you from world space to screen space
 };
 
+// Returns the width and height as a glm::vec2
+inline auto dimensions(const camera& c) -> glm::vec2
+{
+    return {c.screen_width, c.screen_height};
+}
+
 }
