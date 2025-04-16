620e1e4ae7851b1914f79d0dd764a94230f7d020
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2025-04-16 01:46:46 +0100

    Copy a chunk of the shape_renderer into the ui class

--- src/core/ui.cpp
+++ src/core/ui.cpp
@@ -1,28 +1,141 @@
 #include "ui.hpp"
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <ranges>
 
 namespace sand {
+namespace {
 
-ui::ui()
-{
+constexpr auto quad_vertex = R"SHADER(
+    #version 410 core
+    layout (location = 0) in vec2 p_position;
     
+    layout (location = 1) in vec2  quad_centre;
+    layout (location = 2) in float quad_width;
+    layout (location = 3) in float quad_height;
+    layout (location = 4) in float quad_angle;
+    layout (location = 5) in vec4  quad_colour;
+    
+    uniform mat4 u_proj_matrix;
+    
+    out vec4 o_colour;
+    
+    mat2 rotate(float theta)
+    {
+        float c = cos(theta);
+        float s = sin(theta);
+        return mat2(c, s, -s, c);
+    }
+    
+    void main()
+    {
+        vec2 position = quad_centre;
+        vec2 dimensions = vec2(quad_width, quad_height) / 2;
+    
+        vec2 screen_position = rotate(quad_angle) * (p_position * dimensions) + position;
+    
+        gl_Position = u_proj_matrix * vec4(screen_position, 0, 1);
+    
+        o_colour = quad_colour;
+    }
+    )SHADER";
+    
+constexpr auto quad_fragment = R"SHADER(
+    #version 410 core
+    layout (location = 0) out vec4 out_colour;
+    
+    in vec4 o_colour;
+    
+    void main()
+    {
+        out_colour = o_colour;
+    }
+    )SHADER";
+
 }
 
-void ui::start_frame()
+void ui_quad::set_buffer_attributes(std::uint32_t vbo)
 {
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    for (int i = 1; i != 6; ++i) {
+        glEnableVertexAttribArray(i);
+        glVertexAttribDivisor(i, 1);
+    }
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(ui_quad), (void*)offsetof(ui_quad, centre));
+    glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(ui_quad), (void*)offsetof(ui_quad, width));
+    glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(ui_quad), (void*)offsetof(ui_quad, height));
+    glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, sizeof(ui_quad), (void*)offsetof(ui_quad, angle));
+    glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(ui_quad), (void*)offsetof(ui_quad, colour));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 
+ui::ui()
+    : d_shader(quad_vertex, quad_fragment)
+{
+    const float vertices[] = {-1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f};
+    const std::uint32_t indices[] = {0, 1, 2, 0, 2, 3};
+
+    glGenVertexArrays(1, &d_vao);
+    glBindVertexArray(d_vao);
+
+    glGenBuffers(1, &d_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, d_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &d_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, d_ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+}
+
+ui::~ui()
+{
+    glDeleteBuffers(1, &d_ebo);
+    glDeleteBuffers(1, &d_vbo);
+    glDeleteVertexArrays(1, &d_vao);
+}
+
+void ui::start_frame(const camera& c)
+{
+    glBindVertexArray(d_vao);
+    d_quads.clear();
+
+    d_shader.bind();
+    d_shader.load_int("u_camera_width", c.screen_width);
+    d_shader.load_int("u_camera_height", c.screen_height);
+    d_shader.load_vec2("u_camera_top_left", c.top_left);
+    d_shader.load_float("u_camera_world_to_screen", c.world_to_screen);
+
+    const auto dimensions = glm::vec2{c.screen_width, c.screen_height} / c.world_to_screen;
+    const auto projection = glm::ortho(0.0f, dimensions.x, dimensions.y, 0.0f);
+
+    d_shader.bind();
+    d_shader.load_mat4("u_proj_matrix", glm::translate(projection, glm::vec3{-c.top_left, 0.0f}));
 }
 
 void ui::end_frame()
 {
+    glBindVertexArray(d_vao);
 
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    d_shader.bind();
+    d_instances.bind<ui_quad>(d_quads);
+    glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr, (int)d_quads.size());
+
+    glDisable(GL_BLEND);
 }
 
 bool ui::button(glm::vec2 pos, float width, float height)
 {
+    d_quads.emplace_back(pos + glm::vec2{width/2, height/2}, width, height, 0.0f, glm::vec4{1, 0, 0, 1});
     return true;
 }
 
--- src/core/ui.hpp
+++ src/core/ui.hpp
@@ -2,16 +2,47 @@
 #include "buffer.hpp"
 #include "shader.hpp"
 #include "texture.hpp"
+#include "camera.hpp"
 
 #include <glm/glm.hpp>
 
 namespace sand {
 
+// This is just a copy of quad_instance from the shape_renderer, should
+// we combine these? I'm just making a copy now since I am assuming both will
+// iterate in different directions and I don't necessarily want them tied
+// together, but I still feel conflicted.
+struct ui_quad
+{
+    glm::vec2 centre;
+    float     width;
+    float     height;
+    float     angle;
+    glm::vec4 colour;
+
+    static void set_buffer_attributes(std::uint32_t vbo);
+};
+
 class ui
 {
+    std::uint32_t d_vao;
+    std::uint32_t d_vbo;
+    std::uint32_t d_ebo;
+
+    std::vector<ui_quad> d_quads;
+
+    shader d_shader;
+
+    vertex_buffer d_instances;
+
+    ui(const ui&) = delete;
+    ui& operator=(const ui&) = delete;
+
 public:
     ui();
-    void start_frame();
+    ~ui();
+
+    void start_frame(const camera& c);
     void end_frame();
 
     bool button(glm::vec2 pos, float width, float height);
